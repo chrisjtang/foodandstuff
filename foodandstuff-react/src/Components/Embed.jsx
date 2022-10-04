7d2045b18b397244b63b337f50cc73935527bdd6
Chris Tang <chrisjtang@gmail.com>
2022-10-04 15:27:10 -0700

    embed with non-working sso in react.  working sso in html

--- foodandstuff-react/src/Components/Embed.jsx
+++ foodandstuff-react/src/Components/Embed.jsx
@@ -1,17 +1,42 @@
+import React from 'react';
 import { DiscussionEmbed } from 'disqus-react';
 
 const Embed = () => {
-    
+    const handleLogin = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3000/login')
+            .then(response => console.log('response data', response))
+            .then(response => setLoginResponse(response));
+    }
+
     return (
         <div>
+            <div>
+                <button
+                    onClick={handleLogin}
+                >
+                    Login
+                </button>
+            </div>
             <DiscussionEmbed
-                shortname='ctang'
+                shortname='ssoglitch'
                 config={
                     {
-                        url: 'https://ctang-blog.tumblr.com/post/677026795984453632/a-fourth',
-                        identifier: '9040425452',
-                        title: 'test_blog_a_fourth',
-                        language: '',
+                        url: 'https://disqus-sso-demo.glitch.me/',
+                        identifier: '1 https://disqus-sso-demo.glitch.me/',
+                        title: 'Disqus SSO quick start',
+                        remoteAuthS3: '',
+                        apiKey: 'IrsWOMVmYfDfTq7IJ5fXMA2Ix99nB30EF7cP77SDgIrGyoQna1wXwU42wx17xRjN',
+                        sso: {
+                            name:   "SampleNews",
+                            button:  "http://example.com/images/samplenews.gif",
+                            icon:     "http://example.com/favicon.png",
+                            url:        "http://example.com/login/",
+                            logout:  "http://example.com/logout/",
+                            profile_url: "http://example.com/profileUrlTemplate/{username}",
+                            width:   "800",
+                            height:  "400"
+                        },
                     }
                 }
                 />
