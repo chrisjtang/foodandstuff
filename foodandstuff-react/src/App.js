388a71f6fbbc29469aab6564ea2a962653d82ff7
Chris Tang <chrisjtang@gmail.com>
2022-10-05 17:00:44 -0700

    title fixes and explanation of what this app is

--- foodandstuff-react/src/App.js
+++ foodandstuff-react/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Hello, world!
+          Disqus React SSO Demo
         </h1>
+        <h3>This is the React version of <a href='https://disqus-sso-demo.glitch.me/' target='_blank'>https://disqus-sso-demo.glitch.me/</a></h3>
 
       <div>
         <Embed/>
